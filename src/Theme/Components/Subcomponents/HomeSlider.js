ec212f16c5981c1009dc7c17fcae17127bd09a24
akashkharde <akashkharde111@gmail.com>
2023-10-22 10:55:21 +0530

    22/10-1

--- src/Theme/Components/Subcomponents/HomeSlider.js
+++ src/Theme/Components/Subcomponents/HomeSlider.js
@@ -25,7 +25,7 @@ export default function HomeSlider() {
     return (
         <>
             <div data-aos="zoom-out-right" className="" >
-                <Carousel styles={styles} infiniteLoop={true} autoPlay={false} preventMovementUntilSwipeScrollTolerance={true} swipeScrollTolerance={50} >
+                <Carousel styles={styles} infiniteLoop={true} autoPlay={true} preventMovementUntilSwipeScrollTolerance={true} swipeScrollTolerance={50} >
                     <div>
                         <img src={img} className="homeSlider" alt='Img' />
                         <p className="legend">Legend 1</p>
